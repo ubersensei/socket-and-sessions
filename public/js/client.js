229610943d73a34d9acd1c3b01b4c9e80018fea5
Kaya <ubersensei@gmail.com>
2014-08-09 22:34:27 +0800

    ec2-54-179-34-28.ap-southeast-1.compute.amazonaws.com used this instead of localhost

--- public/js/client.js
+++ public/js/client.js
@@ -28,8 +28,10 @@ $(document).ready(function () {
 
 
 //                socket = io('http://localhost', {reconnection: false});
-                  socket = io('http://127.0.0.1', {reconnection: false});
-//                socket = io({reconnection: false});
+//                  socket = io('http://127.0.0.1', {reconnection: false});
+                socket = io('http://ec2-54-179-34-28.ap-southeast-1.compute.amazonaws.com', {reconnection: false});
+
+                socket = io({reconnection: false});
                 var intervalID;
                 var reconnectCount = 0;
 
